0317487e5db529b57de786e870bcfd0ee560e05f
Alejandro-fhnw <alejandro.scheifeledelrio@outlook.com>
2024-10-23 14:45:28 +0200

    #

--- GITLAB/Test1.py
+++ GITLAB/Test1.py
@@ -1,3 +1,13 @@
-import simulator
+import tkinter
+import tkintermapview
 
-simulator.init_logging('data.demo.geojson')
+# create tkinter window
+root_tk = tkinter.Tk()
+root_tk.geometry(f"{1200}x{1000}")
+root_tk.title("map_view_example.py")
+
+# create map widget
+map_widget = tkintermapview.TkinterMapView(root_tk, width=1200, height=1000, corner_radius=0)
+map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+root_tk.mainloop()
